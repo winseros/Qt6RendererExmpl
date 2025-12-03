8b37fc35f0bc0b121025f22db6f7a91810819546
winse <win.accuracy@gmail.com>
2025-12-03 23:26:57 +0300

    Added ambiguous property names to the QJsonObject example

--- types/qjsonobject.h
+++ types/qjsonobject.h
@@ -6,12 +6,14 @@ void DemoQJsonObject() {
 "prop2":1.1,
 "prop3": true,
 "prop4": false,
-"prop5": "value5",
+"type": "value5",
 "prop5a": "2025-10-31T23:59:59+0300",
 "prop6": null,
 "prop7": [1, 1.1, true, false, "value7", null],
 "prop8": {"subProp1": 1, "subProp2": 1.1, "subProp3": [true, false, "subValue8"],
-"subObject": {"subProp4": "value4"}}})");
+"subObject": {"subProp4": "value4"}},
+"type":"aa",
+"size": 1})");
 
     QJsonDocument document1 = QJsonDocument::fromJson(jsonData1);
     QJsonObject obj1 = document1.object();
